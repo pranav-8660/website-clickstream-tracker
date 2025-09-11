870990c0f28b80ab182f5eea5f08053755a9867d
pranav-8660 <pranavcse2003@gmail.com>
2025-09-11 12:11:26 +0530

    starting off with event-collector-service

---
+++ event-collector-service/src/main/java/com/pranavv51/event_collector_service/EventCollectorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.event_collector_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventCollectorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventCollectorServiceApplication.class, args);
+	}
+
+}
