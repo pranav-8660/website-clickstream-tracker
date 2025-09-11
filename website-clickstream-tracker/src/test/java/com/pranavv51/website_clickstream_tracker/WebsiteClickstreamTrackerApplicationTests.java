12a48e564d74a50a133a1a745f73233d35f490ce
pranav-8660 <pranavcse2003@gmail.com>
2025-09-11 12:01:11 +0530

    added base project for starting-up

---
+++ website-clickstream-tracker/src/test/java/com/pranavv51/website_clickstream_tracker/WebsiteClickstreamTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pranavv51.website_clickstream_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteClickstreamTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
