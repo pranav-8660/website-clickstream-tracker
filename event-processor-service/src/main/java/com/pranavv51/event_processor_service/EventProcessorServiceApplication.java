a02416717b1e7091000b7811725f9959b749eacf
pranav-8660 <pranavcse2003@gmail.com>
2025-09-15 11:18:20 +0530

    starting off with event-processor service

---
+++ event-processor-service/src/main/java/com/pranavv51/event_processor_service/EventProcessorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.pranavv51.event_processor_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventProcessorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventProcessorServiceApplication.class, args);
+	}
+
+}
